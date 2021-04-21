26e91afe51ca97f7b8d0a091a4f92471a49843f6
Joel Verhagen <jver@microsoft.com>
2021-04-21 14:01:10 -0700

    Initial oranges commit

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CanHazSourceLink.Oranges
+{
+    public class OrangesClass
+    {
+        public void MyMethod()
+        {
+            if (!string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("ORANGES")))
+            {
+                Console.WriteLine("oranges");
+            }
+        }
+    }
+}
